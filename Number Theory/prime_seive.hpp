eb9cbd3298a044ee67e49a9fc1ee7f77d974a723
Madhur Chauhan <madhur4127@gmail.com>
2020-06-24 15:19:13 +0530

    Seive: Fixed typos and refactored

--- Number Theory/prime_seive.hpp
+++ Number Theory/prime_seive.hpp
@@ -10,16 +10,17 @@ vector<int> prime_seive() {
     vector<int> ret{2}; // add {2} and don't think about even numbers anymore
     if (LIM == 1) return ret;
     ret.reserve(LIM / log(LIM));                     // #primes, pi(x) ~=  x / ln(x)
-    auto bsp = make_unique<bitset<(LIM + 3) / 2>>(); // tackoverflow
+    auto ptr = make_unique<bitset<(LIM + 3) / 2>>(); // stackoverflow
+    auto &bs = *ptr;
     int64_t i = 3, end = (LIM + 1) / 2;
     for (i = 3; i * i <= LIM; i += 2) {
-        if ((*bsp)[(i + 1) / 2]) continue;
+        if (bs[(i + 1) / 2]) continue;
         ret.push_back(i);
         for (int64_t j = (i * i + 1) / 2; j <= end; j += i)
-            (*bsp)[j] = 1;
+            bs[j] = 1;
     }
     for (i = (i + 1) / 2; i <= end; ++i)
-        if (!(*bsp)[i]) ret.push_back(i);
+        if (!bs[i]) ret.push_back(i);
     ret.shrink_to_fit();
     return ret;
 }
